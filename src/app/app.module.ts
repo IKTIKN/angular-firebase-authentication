310b53b3c8423f18a56570d5a2ffd384eb147439
IKTIKN <developermarc@outlook.com>
2023-11-06 19:45:10 +0100

    angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"angular-firebase-auth-37a40","appId":"1:459768664863:web:df6c786db9cdc016a5962a","storageBucket":"angular-firebase-auth-37a40.appspot.com","locationId":"europe-west","apiKey":"AIzaSyCbcE4wmzcIOKZ-ElRlr1ut6sMhJp3K134","authDomain":"angular-firebase-auth-37a40.firebaseapp.com","messagingSenderId":"459768664863","measurementId":"G-2X3ZH1HLHX"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,
+    UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
